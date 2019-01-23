1e191808b3d36f1dd63baaed9cecf4bdec8c16d4
Jairo <jairobernaldev@gmail.com>
2019-01-23 21:42:57 +0100

    Calendario

--- EjerciciosT4/ejercicio.js
+++ EjerciciosT4/ejercicio.js
@@ -1,24 +1,24 @@
 var http = require("http");
 var url = require("url");
 
-http.createServer(function(request, response){
-    response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+http.createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
 
     var ca = url.parse(request.url, true)
     response.end(peticion(ca.pathname, ca.query));
 
-}).listen(8080,"localhost");
+}).listen(8080, "localhost");
 console.log("Servidor corriendo en http://localhost:8080");
 
-function peticion(path,q){
-    switch (path){
+function peticion(path, q) {
+    switch (path) {
         case "/tablas":
             var ta;
-            if(q.num != undefined){
+            if (q.num != undefined) {
                 ta = ej2(q.num);
-            }else if(q.min != undefined && q.max != undefined){
+            } else if (q.min != undefined && q.max != undefined) {
                 ta = ej3(q.min, q.max);
-            }else{
+            } else {
                 ta = ej1();
             }
             return ta;
@@ -27,45 +27,45 @@ function peticion(path,q){
             return ej4(q.num1, q.num2, q.op);
 
         case "/calendario":
-            return ej5(q.año,q.mes);
+            return ej5(q.año, q.mes);
         default:
             return ej1();
     }
 }
 
-function ej1(){
+function ej1() {
     var re = "";
-    for(var i = 0; i<= 10; i++){
-        for(var j = 0; j<=10; j++){
-            re += i+" * "+j+" = "+(i*j)+"<br>";
+    for (var i = 0; i <= 10; i++) {
+        for (var j = 0; j <= 10; j++) {
+            re += i + " * " + j + " = " + (i * j) + "<br>";
         }
         re += "<br>";
     }
     return re;
 }
 
-function ej2(nun){
+function ej2(nun) {
     var re = "";
-    for(var i = 0; i<= 10; i++){
-        re += nun+" * "+1+" = "+(nun*i)+"<br>";
+    for (var i = 0; i <= 10; i++) {
+        re += nun + " * " + 1 + " = " + (nun * i) + "<br>";
     }
     return re;
 }
 
-function ej3(min, max){
+function ej3(min, max) {
     var re = "";
-    for(var i = min; i<= max; i++){
-        for(var j = 0; j<=10; j++){
-            re += i+" * "+j+" = "+(i*j)+"<br>";
+    for (var i = min; i <= max; i++) {
+        for (var j = 0; j <= 10; j++) {
+            re += i + " * " + j + " = " + (i * j) + "<br>";
         }
         re += "<br>";
     }
     return re;
 }
 
-function ej4(num1, num2, op){
+function ej4(num1, num2, op) {
     var re;
-    switch(op){
+    switch (op) {
         case "suma":
             re = "" + (parseInt(num1) + parseInt(num2));
             break;
@@ -85,6 +85,80 @@ function ej4(num1, num2, op){
     return re;
 }
 
-function ej5(anyo, mes){
+function ej5(anio, mes) {
+    var dia = "1";
+    var dias = ["dom", "lun", "mar", "mie", "jue", "vie", "sab"];
+    var dt = new Date(mes + ' ' + dia + ', ' + anio);
+    //var text = "Dia de la semana : " + dias[dt.getUTCDay()];
+    var diaSemana = true;
 
+    var calendario = "<table><tr><th>" + dias[0] + "</th><th>" + dias[1] + "</th><th>" + dias[2] + "</th><th>" + dias[3] + "</th><th>" + dias[4] + "</th><th>" + dias[5] + "</th><th>" + dias[6] + "</th></tr>";
+    var diasdemes = diasDelMes(mes);
+    for (var i = 1; i <= (diasdemes + 1); i++) {
+        calendario += "<tr>"
+        for (var j = 0; j < 7; j++) {
+            if (diaSemana) {
+                if (dias[j] == dias[dt.getUTCDay()]) {
+                    diaSemana = false;
+                    calendario += "<td></td>";
+                    //i++;
+                } else {
+                    calendario += "<td></td>";
+                }
+            } else {
+                if (i != (diasdemes + 1)) {
+                    calendario += "<td>" + i + "</td>";
+                    i++;
+                }
+            }
+        }
+        calendario += "</tr>"
+    }
+    calendario += "</table>"
+
+
+    return calendario;
+}
+
+function diasDelMes(mes) {
+    switch (mes) {
+        case "1":
+            return 31;
+            break;
+        case "2":
+            return 28;
+            break;
+        case "3":
+            return 31;
+            break;
+        case "4":
+            return 30;
+            break;
+        case "5":
+            return 31;
+            break;
+        case "6":
+            return 30;
+            break;
+        case "7":
+            return 31;
+            break;
+        case "8":
+            return 31;
+            break;
+        case "9":
+            return 30;
+            break;
+        case "10":
+            return 31;
+            break;
+        case "11":
+            return 30;
+            break;
+        default:
+            return 31;
+            break;
+
+
+    }
 }
